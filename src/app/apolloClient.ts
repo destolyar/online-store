a2459ce1131924d4c387b69f9bcb236cdbdf2c9b
destolyar <dead.stolyar@gmail.com>
2022-08-09 19:49:22 +0400

    fetch settings

--- src/app/apolloClient.ts
+++ src/app/apolloClient.ts
@@ -1,9 +1,15 @@
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
 
 export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: new InMemoryCache({}),
   uri: 'https://online-store-endpoints.herokuapp.com/',
   headers: {
     "Access-Control-Allow-Origin": "*"
-  }
+  },
+  link: new HttpLink({
+    uri: 'https://online-store-endpoints.herokuapp.com/',
+    fetchOptions: {
+      mode: 'no-cors'
+    }
+  })
 })
