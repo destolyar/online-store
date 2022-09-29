54a5b275ca514786661538f56cf4e4dc6d308abe
destolyar <dead.stolyar@gmail.com>
2022-09-29 15:38:18 +0400

    Revert "uri"
    This reverts commit 82dd5ec3882a136c17d6dcc6342d3a2c03e9dc4e.

--- src/app/apolloClient.ts
+++ src/app/apolloClient.ts
@@ -2,14 +2,5 @@ import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "@a
 
 export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: new InMemoryCache({}),
-  uri: 'https://online-store-endpoints.herokuapp.com/',
-  headers: {
-    "Access-Control-Allow-Origin": "*"
-  },
-  link: new HttpLink({
-    uri: 'https://online-store-endpoints.herokuapp.com/',
-    fetchOptions: {
-      mode: 'no-cors'
-    }
-  })
+  uri: 'https://online-store-endpoints.herokuapp.com/'
 })
